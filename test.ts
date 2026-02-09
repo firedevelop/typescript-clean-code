4d80157443723e8ac24bfac3901139fc9b10a32b
Manuel encodeBiz <manuel@encodebiz.com>
2026-02-09 11:18:45 +0100

    docs: section 9

--- test.ts
+++ test.ts
@@ -1,10 +1,27 @@
-let frutas =["p","m"];
-let peras = frutas[0]
-console.log(peras)
+(() => { 
+    abstract class Person{
+        constructor(
+            public name:string,
+            public country:string
+        ){}
+        abstract showRole():void,
+        welcome(){
+            return `bienvenido, ${this.name} es de ${this.country}`;
+        }
+    }
 
 
-let frutas2:string[]=["p",'m'];
-
-let [pera,manzana]=frutas2;
-
-console.log(pera)
+    class Employee extends Person{
+        constructor(
+            name: string,
+            country:string,
+            public jobTitle: string
+        ){
+            super(name, country);
+        }
+        showRole(){
+             .log()
+        }
+    }
+ })()
+ 
